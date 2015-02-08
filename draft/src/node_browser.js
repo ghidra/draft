050ff5623a2c70fe208fdeac9a2d031c63927d37
ghidra <ghidra@gmail.com>
2015-02-08 00:41:27 -0500

    better menu node creation

--- draft/src/node_browser.js
+++ draft/src/node_browser.js
@@ -3,53 +3,67 @@ draft.node_browser=function(layer){
 }
 
 draft.node_browser.prototype.init=function(layer){
-	this.menu = {};
-	
-	/*var menu_container = document.createElement("DIV");
-	menu_container.id = "node_menu_list";
-	menu_container.class="node_main_menu_label";
+	this.menu = {};//these will be used for searching
+	this.menu_categories = {};
+	this.visible=false;
+	var _this = this;
 
-	var menu_label = document.createElement("DIV");
-	menu_label.innerHTML = "TAB Menu";
-	menu_label.class="node_main_menu_label";
-
-	menu_container = ;*/
-	
-	//this.menu_html = "<div id=\"node_menu_list\"><div class=\"node_main_menu_label\">TAB Menu</div>";
-	this.menu_categories_html = {};
-
-
+	//set the passed in layer to hang this all on
 	this.canvas = document.getElementById(layer);
 	this.canvas.style.visibility="hidden";
 	this.canvas.style.position = "absolute";
 	this.canvas.style.color = 0x0;
 
-	this.visible=false;
+	//the main container
+	var menu_container = document.createElement("DIV");
+	menu_container.id = "node_menu_list";
+
+	//the label
+	var menu_label = document.createElement("DIV");
+	menu_label.innerHTML = "TAB Menu";
+	menu_label.className="node_main_menu_label";
+
+	menu_container.appendChild(menu_label);
 
 	for (var category in draft.nodes){
 		if(draft.nodes.hasOwnProperty(category)){
-			
-			this.menu[category]={};//set the category
-			//set the main html parts
-			this.menu_html+="<a id=\"node_main_menu_"+category+"\" class=\"node_menu_label\">"+category+"</a>";
-			//make a category html element
-			this.menu_categories_html[category]="<div id=\"node_sub_menu_"+category+"\" class=\"node_menu_list\">";
 
+			this.menu[category]={};//set the category
+
+			//set the main menu parts
+			var menu_item = document.createElement("A");
+			menu_item.id = "node_main_menu_"+category;
+			menu_item.className="node_menu_label";
+			//i need to create a closure here, because the varialbe is wrong
+			menu_item.onmouseover=function(){_this.show_sub_menu(category)};
+			menu_item.innerHTML=category;
+			menu_container.appendChild(menu_item);
+
+			//make a category container element
+			var sub_menu_container = document.createElement("DIV");
+			sub_menu_container.id = "node_sub_menu_"+category;
+			sub_menu_container.className = "node_menu_list";
+
+			//now add to the sub container
 			for (var n in draft.nodes[category]){
 				if(draft.nodes[category].hasOwnProperty(n)){
 					this.menu[category][n] = n;
 					//set the secondary html parts
-					this.menu_categories_html[category]="<a id=\"node_sub_menu_"+category+"_"+n+"\" class=\"node_menu_label\">"+n+"</a>";
+					var sub_menu_item = document.createElement("A");
+					sub_menu_item.id = "node_sub_menu_"+category+"_"+n;
+					sub_menu_item.className = "node_menu_label";
+					sub_menu_item.onmousedown=function(){_this.create_node(category,n)};
+					sub_menu_item.innerHTML=n;
+
+					sub_menu_container.appendChild(sub_menu_item);
 				}
 			}
-			//close the sub menu category
-			this.menu_categories_html[category]+="</div>"
+			//
+			this.menu_categories[category]=sub_menu_container;
 		}
 	}
 
-	this.menu_html+="</div>"
-
-	this.canvas.innerHTML = this.menu_html;
+	this.canvas.appendChild(menu_container);
 
 	return this;
 }
@@ -65,7 +79,12 @@ draft.node_browser.prototype.toggle=function(p){
 		this.visible=true;
 	}
 }
-
+draft.node_browser.prototype.show_sub_menu=function(sub){
+	alert(sub);
+}
+draft.node_browser.prototype.create_node=function(sub,node){
+	alert(sub+":"+node);
+}
 draft.node_browser.prototype.test=function(){
 	alert("something");
 }
