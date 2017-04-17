1472affe475826452f0da1863babaf2108503a3e
ghidra <ghidra@gmail.com>
2017-04-17 18:15:08 -0400

    trying to get marquee to behave correctly

--- draft/src/marquee.js
+++ draft/src/marquee.js
@@ -43,10 +43,15 @@ draft.marquee.prototype.start_drag=function(mp){
 }
 draft.marquee.prototype.mouseover=function(e){
 	var mp = rad.relativemouseposition(e);
+
+	var w = mp.x-2;
+	var h = mp.y-2;
+
 	this.canvas.style.width = mp.x-2;
 	this.canvas.style.height = mp.y-2;
 	
-	this.bottomcorner = new rad.vector2(mp.x-2,mp.y-2);
+	this.set_bottomcorner(w,h);
+	//this.bottomcorner = new rad.vector2(mp.x-2,mp.y-2);
 }
 draft.marquee.prototype.mouseup=function(){
 	this.remove();
@@ -54,31 +59,60 @@ draft.marquee.prototype.mouseup=function(){
 }
 draft.marquee.prototype.drag=function(mp){
 	//now set the width of the later
-	this.canvas.style.width = mp.x-this.corner.x-2;
-	this.canvas.style.height = mp.y-this.corner.y-2;
+	var w = mp.x-this.corner.x-2;
+	var h = mp.y-this.corner.y-2;
+	
+	this.canvas.style.width = w;
+	this.canvas.style.height = h;
 
-	this.bottomcorner = new rad.vector2( mp.x-this.corner.x-2, mp.y-this.corner.y-2 );
+	this.set_bottomcorner(w,h);
+
+	//this.bottomcorner = new rad.vector2( mp.x-this.corner.x-2, mp.y-this.corner.y-2 );
 }
 draft.marquee.prototype.remove=function(){
 	//rad.unbind(this.canvas,"mousemove",rad.closure(this,this.over) );
 	this.canvas.style.visibility="hidden";
 	this.visible=false;
 
-	//add nodes inside the marquee to the selection
+	//THIS IS WHERE WE ADD NODES IN THAT ARE INSIDE THE MARQUEE
+	//we should get the scaled offest relative position of the marquee to the graph
+	var cpos = rad.domposition(draft.canvas) ;
+	var cscl = draft.scripts[draft.activescript].scale.scale ;
+
+	var offpos = this.p.sub(cpos);
+	var offcorner = this.bottomcorner.sub(cpos);
+	
+	//console.log("canvas x:"+cpos.x+" y:"+cpos.y);
+	console.log("-------------");
+	console.log("MARQUEE DATA");
+	console.log( "x:"+this.p.x+"\ny:"+this.p.y+"\nbottom corner x:"+this.bottomcorner.x+"\nbottom corner y:"+this.bottomcorner.y );
+
+	console.log("GRAPH DATA");
+	console.log("scale:"+cscl);
 	//loop the nodes...
 	//deal with the node
 	for (var n in draft.get_active_nodes() ){//loop all the nodes
 		var nd = draft.get_active_node(n);
 		//get the node dimensions
 		//check x
-		var xa = (nd.p.x>this.p.x && nd.p.x<this.bottomcorner.x);
-		var xb = (nd.bottomcorner.x>this.p.x && nd.bottomcorner.x<this.bottomcorner.x);
-		var ya = (nd.p.y>this.p.y && nd.p.y<this.bottomcorner.y);
-		var yb = (nd.bottomcorner.y>this.p.y && nd.bottomcorner.y<this.bottomcorner.y);
+		var xa = (nd.p.x>this.p.x && nd.p.x<this.bottomcorner.x);//x top corner
+		var ya = (nd.p.y>this.p.y && nd.p.y<this.bottomcorner.y);//y top corner
+		
+		var xb = (nd.bottomcorner.x>this.p.x && nd.bottomcorner.x<this.bottomcorner.x);//x bottom corner
+		var yb = (nd.bottomcorner.y>this.p.y && nd.bottomcorner.y<this.bottomcorner.y);//y bottom corner
+		
+		var xc = (nd.bottomcorner.x>this.p.x && nd.bottomcorner.x<this.bottomcorner.x);//x top right
+		var yc = (nd.p.y>this.p.y && nd.p.y<this.bottomcorner.y);//y top right
+		
+		var xd = (nd.p.x>this.p.x && nd.p.x<this.bottomcorner.x);//x bottom left
+		var yd = (nd.bottomcorner.y>this.p.y && nd.bottomcorner.y<this.bottomcorner.y);//y bottom left
 
-		console.log("xa:"+nd.p.x+" my p:"+this.p.x+" bottom corner:"+this.bottomcorner.x)
-		console.log("RESULTS"+xa+":"+xb+":"+ya+":"+yb);
-		if (xa || xb || ya || yb) {
+		console.log("NODE DATA");
+		console.log("xa:"+nd.p.x+"\nya:"+nd.p.y)
+		console.log("RESULTS"+ (xa&&ya) +":"+ (xb&&yb) +":"+ (xc&&yc) +":"+ (xd&&yd) );
+		
+		//if ( (xa && ya) || (xb && yb) || (xc && yc) || (xd && yd) ) {
+		if ( (xb && yb) ) {
 			nd.selected=true;	
 		}
 		
@@ -87,7 +121,10 @@ draft.marquee.prototype.remove=function(){
 	}
 	draft.refresh();
 }
-
+draft.marquee.prototype.set_bottomcorner=function(w,h){
+	//helper function to get the correct corner position
+	this.bottomcorner = new rad.vector2( w+this.corner.x, h+this.corner.y );
+}
 draft.marquee.prototype.test=function(){
 	alert("something");
 }
